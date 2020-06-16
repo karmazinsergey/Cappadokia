a03316085a14a589246cdc4119677756a717dd56
karmazinsergey <karmazins92@yandex.ru>
2020-06-16 20:58:23 +0300

    Media commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-    $('.fairy-tail__slider').slick({
+    $(".fairy-tail__slider").slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
         /* автоматическое проигрывание*/
@@ -7,9 +7,17 @@ $(function() {
         /* скорость проигрывания */
         autoplaySpeed: 3000,
         /* анимация перелистывания слайдера */
-        fade: true
+        fade: true,
+        responsive: [{
+            breakpoint: 601,
+            settings: {
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+            },
+        }, ],
     });
-    $('.our-trip__slider').slick({
+    $(".our-trip__slider").slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
         /* автоматическое проигрывание*/
@@ -17,6 +25,14 @@ $(function() {
         /* скорость проигрывания */
         autoplaySpeed: 3000,
         /* анимация перелистывания слайдера */
-        fade: true
+        fade: true,
+        responsive: [{
+            breakpoint: 601,
+            settings: {
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+            },
+        }, ],
     });
 });
